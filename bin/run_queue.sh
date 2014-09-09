abf50218d1f0c65cb8af1a9165a26de64932b8a6
atupal <atupalykl@gmail.com>
2014-09-09 14:53:48 +0000

    use exec to start celely, so can use docker kill -s INT to send signal to celery process

--- bin/run_queue.sh
+++ bin/run_queue.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-C_FORCE_ROOT=1 celery -A celeryapp worker -l info -c $3 -P gevent --soft-time-limit 10 --without-gossip  -n $1 -Q $2
+# no -l info
+C_FORCE_ROOT=1 exec celery -A celeryapp worker  -c $3 -P gevent --soft-time-limit 10 --without-gossip  -n $1 -Q $2
