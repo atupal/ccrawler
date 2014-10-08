d2a1ba903c1649d42f32c9b55ef3ae7487120d70
atupal <atupalykl@gmail.com>
2014-10-08 17:01:03 +0000

    update exmaple crawler code

--- bin/newcrawler.py
+++ bin/newcrawler.py
@@ -31,22 +31,25 @@ def compress(filepath):
     return retstring
 
 TEMPLATE_CRAWLER = convert("""
-eJyVVDtv2zAQ3vUrCHcgZcj00A6FUS8tUHQsmjEICEY62YwpkuYDiRG0v72k3oqCAuWi4919353u
-wQ9ot92hUldCnQ4o+Hr3OWmyzWaToXj+jKdVicZo65F2g+RuLiuDtaA8M9yf0TEaaZJoJaziDZDh
-zh9d+hLGaiGBsTzPIrq3GQOqGl2ftFBkTlsgTCn+b8S+A2W11Q2ycA3gPH3kDn518g+uKgkW9T/z
-dWXpkIZbBy3uZ5LeQc31HcaVZ6iC7GB3/WWBGZUdoOKeJ98ujjAghYJFjF6XDbX3ooFBjmiY35+c
-Vh3vo/s0sgAPXtRB3ulgsqyU3Dm0/GGyrkF+aCehghqdWw1xIOsCee4uBQoOmLH65Xb8zqWDAm23
-l2duT66HpZM842C4YGKAJXmBEllOe+bkmY84Cz5Y1cKHbOeFJm8r/69MZ+nYmEtS3WMLzmjlAD+M
-RhdLE+2LUhFLPbz4Am3kSyM3U4IKnllichFxP3GIuucPVrIKjD/jB/QFfZxSSKfWFsV+XpBQbVRa
-C1UxLiXBHOdL35bVQxPjvCZWfGih9ASe4LOFGudx4Kdwh3UCv1eEY/bDMqUI71a/mHyHRozTSxaz
-PGvBCRRY7rVtu7Aeh9dVQv2f4bP35rDfX6+01A1euU1/chMgq+V89EtC5hszSyqOagnO9YMR+x+k
-n49qaopNHelNyzYkFDVWxDemsxMbX5fYbsbSU8cYOh4RZqzhQjGGO3C3hUJR53nbYK2A2aDmu/3N
-8uf0EiT/spPTEE6WN2tDYrsXqxDvU0eScahDns9JaYxL8uwvzejo6Q==
+eJyVVMtq3DAU3fsrxHQhOTgOlC7K0Nm0ULoszTIEodjXM0pkSdGDmSG0394rv50ZCvXGku4596Vz
+9YHc3tySytRS77ckhub2czrJNptNRvCDk2itgtKeu+2f6esQsrXGBWL8uPJnn1XROdCBWxEOZIfG
+Mq3KWjotWmDjXjz59GecN1IB53meIXuwWQu6nqDPRmq2dFsQWpb0vxl3PSlrnGmJg9cIPpRPwsOv
+fv1D6FqBI0MxXy8sPdMK56Hj/UyrK6zlec/x1QHqqHra/bBZcabDnlCLIBK2jyMtKKlhFWM4y8be
+B9nCuEY2LPfP3uje75P/NHkBEYNsoro30WZZpYT3ZF0wu+xBvu2UUENDDt0J86CaggThXwoSPXDr
+zOm8+y6Uh4Lc3Lwchdv7gZa+hERh+GgxwNp5QZKzvBw8J2Q+8RyE6HRHH7NdNpq97/y/Ml2k4zCX
+dPRAHXhrtAf6+EArowOKhz5OOI9dQuiqawwz3qhTqzZzmhqOPPnzCH6Y6bIZokSneA02HOgj+UI+
+zomkrzGO4K2+EKm7gGUjdc2FUowKmq+xg9cEL/cQGD04aCgqnealD8IFf5Q4YPQQgqU5wRYQbcJ1
+PA7PgIYTXAvUBQvQYlFvqQS6fe8oR09zbdvLan9fdTq1a5zhFOXqpRczdrz/aWjYaoQWNy+1DHwP
+GpwIxnUKuJTi20VmQ4ld77Z3d6+vZWVaWlzg5prOElS9FucwoWw5rovUcE4q8H5QJYovqrCck6QF
+l4QwmNaXklildRIfuN7OXL4CWMwBH4eGcJ4eXs7Jbkco562QmnPae+vfBKmTYDqhGQ3cRb18ab45
+cUzvUsJX/TrNwWx5N8QMlbAaTNzPF5WMY2PyfOm0xLgsz/4Cf7kUfA==
 """)
 
 def main():
     args = parser.parse_args()
-    print args.name
+    print '\033[31m', 'generating crawler: %s' % args.name, '\033[0m'
+    with open(os.path.join(root, 'crawlers/%s.py' % args.name), 'w') as fi:
+        fi.write(TEMPLATE_CRAWLER)
 
 if __name__ == '__main__':
     main()
